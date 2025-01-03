cb3a3e58d423ccdc4ce320d91d3aef040f4110dd
bsaragiotto <bruno.saragiotto@gmail.com>
2025-01-03 20:39:21 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,17 +16,17 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
-    if "messages" not in st.session_state:
+if "messages" not in st.session_state:
         st.session_state.messages = []
 
     # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
+for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("What is up?"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
