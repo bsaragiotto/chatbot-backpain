25fa65e2386df9c4670a1b8e55fa051a9db107fc
bsaragiotto <bruno.saragiotto@gmail.com>
2025-01-03 20:51:28 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,36 +4,33 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chat-Back")
 st.write(
-    "Welcome to Chat-Back! 👋 I'm your friendly assistant here to help you understand and manage back pain. Whether you're looking for tips, exercises, or general advice, I'm here to provide guidance and support."
-    "Ask me anything about back pain"
+    "Welcome to Chat-Back! 👋 I'm your friendly assistant here to help you understand and manage back pain. "
+    "Whether you're looking for tips, exercises, or general advice, I'm here to provide guidance and support. "
+    "Ask me anything about back pain."
 )
 
-# .streamlit/secrets.toml
-#OPENAI_API_KEY = st.secrets["openai"]["api_key"]
-
-    # Set OpenAI API key from Streamlit secrets
+# Set OpenAI API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
 if "messages" not in st.session_state:
-        st.session_state.messages = []
+    st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
+# Display the existing chat messages via `st.chat_message`.
 for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
+# Create a chat input field to allow the user to enter a message.
 if prompt := st.chat_input("What is up?"):
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
+    # Generate a response using the OpenAI API.
+    try:
         stream = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -48,3 +45,5 @@ if prompt := st.chat_input("What is up?"):
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
